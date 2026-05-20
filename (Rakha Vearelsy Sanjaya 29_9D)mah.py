8b5ce169bd06fe155c008b6e38f98745b58d0c6f
tiksmpn4depok <tiksmpn4depok@gmail.com>
2026-05-20 15:11:04 +0700

    Add files via upload

---
+++ (Rakha Vearelsy Sanjaya 29_9D)mah.py
@@ -0,0 +1,28 @@
+# Ini untuk mencetak
+print("Rakha")
+print(67)
+
+# Operator aritmatika
+print(6+7)
+print(6-7)
+print(6*7)
+print(6/7)
+print(6**7)
+print(21//6)
+print(6%2)
+
+nama='Rakha'
+nama='Sanjaya'
+print(nama)
+
+Nama='Rakha'
+print(Nama)
+
+angka1= 10
+angka2= 20
+print(angka1+angka2)
+
+nama_depan='Rakha'
+nama_belakang='Sanjaya'
+print(nama_depan +' '+ nama_belakang)
+print(nama_depan,nama_belakang)
